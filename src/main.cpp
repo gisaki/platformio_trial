bcbda1b956471b89efb388f9c16b54f2bab9b80e
gisaki <gisaki.dev@gmail.com>
2026-02-14 17:55:28 +0900

    [update] サンプルとして M5StickCの初期化と画面表示の追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <M5StickC.h> // M5StickCPlusの場合は M5StickCPlus.h
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // M5StickCの初期化
+    M5.begin();
+    
+    // 画面
+    M5.Lcd.begin();
+    M5.Lcd.setRotation(3);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setTextColor(WHITE, BLACK);
+
+    // テキスト
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.println("Hello M5StickC");
+    M5.Lcd.print("PlatformIO");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // 必要に応じてループ処理
 }
